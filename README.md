8ccf3f554c45cf37cb694c05c5277ab5b618fe82
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-10-20 15:57:12 -0400

    attempted two methods of generated randomness by putting arrays in an object, came close but couldn't get the values right

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# passwordGenerator
+# Password Generator Project
+    The project's purpose is to have an user click on a button and have the page ask them what dimensions they want their password to be. Then to generate and display that information to the user.
+# Motivation
+    I wanted to complete this project so that I could have a better understanding of java and its functions.
+# What I Learned
+    This was much more difficult than I thought it would be. My initial build was to for loop with if statements covering up every possible outcome. But then I read documentation about continues and breaks and thought I could use those to skip the entries that the user didn't want. I tried mulitple different approaches that are commented in the gen.js file but in the end, I could not figure out just how to take into case every outcome without multiple if statements. 
+
+    One new thing I did learn was programing the button to cause the comforms and prompts to trigger. Another useful thing I learned was .slice, a why to take out portions of an array that you want. I'm sure it would be helpfull in this project but I still strugged with figuring out how to delineate all of the inputs from the user to ultimatly it was moot.  I attempted to make the output of all the the statements display on the page. I'm sure I had the right selectors but It just would't appear.
+
+    Bootstrap was FAR easier this time around and I'm very please with how the page looks and its responsiveness.
+# Conclusion
+    My take away from this project is; I really wish that I had someone to bounce ideas off of. Several times I felt like I was on the right track but couldn't quite piece it together and just ended up with subpar code. I really believe that at least talking it over with classmates would have help formulate ideas. Its tough to do this on your own when you are so unsure of how it will work and what to do when it doesn't.
--- java/gen.js
+++ java/gen.js
@@ -1,54 +1,96 @@
 //---- Getting the information
-var askLength = prompt("Select a character lenght between 8 and 128")
-var askSpecial = confirm("Would you like special characters in your password")
-var askLower = confirm("Would you like lower case characters in your password")
-var askUpper = confirm("Would you like upper case characters in your password")
-var askNum = confirm("Would you like numbers in your password")
+document.getElementById("genButton").addEventListener("click",function(){
+    var askLength = prompt("Select a character lenght between 8 and 128")
+    var askSpecial = confirm("Would you like special characters in your password")
+    var askLower = confirm("Would you like lower case characters in your password")
+    var askUpper = confirm("Would you like upper case characters in your password")
+    var askNum = confirm("Would you like numbers in your password")
 
+    var passpass = [] //to return the loops to user
+    
+    // codes for the different conditions
+    
+    if (askNum === false) {
+        for (var i = 0; i < askLength; i++) {
+            var passDone = notNum[Math.floor(Math.random() * notNum.length)];
+            passpass.push(passDone);
+            alert(passpass);
+            console.log(passDone); 
+        }
+    }
+    else if (askSpecial === false) {
+        for (let i = 0; i < askLength; i++) {
+            var passDone = notSpec[Math.floor(Math.random() * notSpec.length)];
+            passpass.push(passDone);
+            alert(passpass);
+            console.log(passDone)
+        }
+    }
+    else if (askLower === false) {
+        for (let i = 0; i < askLength; i++) {
+            var passDone = notLower[Math.floor(Math.random() * notLower.length)];
+            passpass.push(passDone);
+            alert(passpass);
+            console.log(passDone)
+        }
+    }
+    else if (askUpper === false) {
+        for (let i = 0; i < askLength; i++) {
+            var passDone = notCap[Math.floor(Math.random() * notCap.length)];
+            passpass.push(passDone);
+            alert(passpass);
+            console.log(passDone)
+        }
+    }
+    else {
+        alert("Must select password parameters")
+    }
+})
 
+// ---- array for different answers
 var notNum = ["q", "w", "e", "r", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m",
     "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M",
     "!", "@", "#", "$", "%", "^", "&", "*"]
 var notSpec = ["q", "w", "e", "r", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m",
     "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M",
     1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-var notLower = [, "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M",
+var notLower = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M",
     "!", "@", "#", "$", "%", "^", "&", "*",
     1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var notCap = ["q", "w", "e", "r", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m",
     1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
     "!", "@", "#", "$", "%", "^", "&", "*"]
 
-if (askNum === false) {
-    for (var i = 0; i < askLength; i++) {
-        var passDone = notNum[Math.floor(Math.random() * notNum.length)];
-        console.log(passDone)
-    }
-}
-else if (askSpecial === false) {
-    for (let i = 0; i < askLength; i++) {
-        var passDone = notSpec[Math.floor(Math.random() * notSpec.length)];
-        console.log(passDone)
-
-    }
-}
-else if (askLower === false) {
-    for (let i = 0; i < askLength; i++) {
-        var passDone = notLower[Math.floor(Math.random() * notLower.length)];
-        console.log(passDone)
-    }
-}
-else if (askUpper === false) {
-    for (let i = 0; i < askLength; i++) {
-        var passDone = notCap[Math.floor(Math.random() * notCap.length)];
-        console.log(passDone)
-    }
-}
-else {
-    alert ("Must select password parameters")
-}
 
 
 
 
+// //--- Testing some ideas
+// by puting the arrays in an object, I could skip over the one I dont want to include in the password.
+// var passWord = {
+//     num: passNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+//     sym: passSym = ["!", "@", "#", "$", "%", "^", "&", "*", ":"],
+//     up: passUp = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"],
+//     low: passLow = ["q", "w", "e", "r", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+// }
 
+// var passwordAnswer = [] // the return array
+
+// if (askNum === false) {
+//     let i = 0
+//     do {
+//         //tyring to select a random array in the passWord object and a random index from that random array.
+//         var pass = passWord[Math.floor(Math.random() * 10)];
+//         // var something = pass[Math.floor(Math.random())];
+//         // // then return that into passwordAnswer to then display to the user
+//         // passwordAnswer.push(something);
+//         // alert(passwordAnswer)
+//         // console.log(something);
+//         // console.log(pass)
+//         i++;
+//     }
+//     while (i <= askLength)
+// }
+
+
+//conclusion: I still can't solve the issue of the user selecting multiple options let alone fairly generate a password for those options.
